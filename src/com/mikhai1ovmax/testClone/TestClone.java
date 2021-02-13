e508a853f87b4fb9499f7f5399aa30e7a50af232
mikhai1ovMax <mikhai1ovMax@yandex.ru>
2021-02-13 13:09:16 +0300

    test clone method

--- src/com/mikhai1ovmax/testClone/TestClone.java
+++ src/com/mikhai1ovmax/testClone/TestClone.java
@@ -1,6 +1,6 @@
 package com.mikhai1ovmax.testClone;
 
-class TestClone implements Cloneable {
+class TestClone implements Cloneable{
     String text;
 
     TestClone() {}
@@ -10,10 +10,8 @@ class TestClone implements Cloneable {
         this.text = text;
     }
 
-    public static void main(String[] args) throws CloneNotSupportedException {
-        TestClone t1 = new TestClone("Abc");
-        TestClone t2 = (TestClone) t1.clone();
-        System.out.println(t1 + " " + t1.text);
-        System.out.println(t2 + " " + t2.text);
+    @Override
+    protected TestClone clone() throws CloneNotSupportedException {
+        return new TestClone(text);
     }
 }
